bd7caf1653a381725bfe980ceb43f7f00bf2c02a
yess2992 <yess2992@gmail.com>
2023-07-23 01:11:36 -0500

    Arrocitas

---
+++ Documents/SharpDevelop Projects/Ejercicios POO1/Ejercicios POO1/Class1.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Yésica Gutiérrez
+ * Fecha: 25/08/2021
+ * Hora: 11:39 a. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Ejercicios_POO1
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public class Class1
+	{
+		public Class1()
+		{
+		}
+	}
+}
---
+++ Documents/SharpDevelop Projects/poo/ejercicio1/Trabajador.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Yésica Gutiérrez
+ * Fecha: 31/08/2021
+ * Hora: 6:04 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace ejercicio1
+{
+	/// <summary>
+	/// Description of Trabajador.
+	/// </summary>
+	public class Trabajador
+	{
+		public Trabajador()
+		{
+		}
+	}
+}
